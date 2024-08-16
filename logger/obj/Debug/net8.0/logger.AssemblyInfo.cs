bde2c1a1eb700f47656cf3d8e6b19d717aca85fc
asknvl <asknvl@gmail.com>
2024-08-16 14:56:46 +0300

    wip

--- logger/obj/Debug/net8.0/logger.AssemblyInfo.cs
+++ logger/obj/Debug/net8.0/logger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("logger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023a1f7eb835ccff5ab84983fab1f2abbde2dca7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b14b74c071e04876c7831db7ca2cfb627b560f33")]
 [assembly: System.Reflection.AssemblyProductAttribute("logger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("logger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
