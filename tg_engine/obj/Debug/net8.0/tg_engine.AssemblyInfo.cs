7b8dd1d744bfc134336db33ef5c9b827b9c08606
Alexey Konovalov <asknvl@gmail.com>
2024-09-22 01:37:41 +0300

    wip

--- tg_engine/obj/Debug/net8.0/tg_engine.AssemblyInfo.cs
+++ tg_engine/obj/Debug/net8.0/tg_engine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tg_engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.7.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.7+bf90aea57b3303509b14b5ce5e1a38b02802a732")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.7+a61b9d85c2d8fcc17346d6c798b58b965e0be641")]
 [assembly: System.Reflection.AssemblyProductAttribute("tg_engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tg_engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.7.0")]
